c722eda78da533d956f64b8d3b2aea079969b562
alexkonkin <alexkonkin@gmail.com>
2021-05-13 19:21:20 +0300

    Update run_hw.sh

--- provisioners/run_hw.sh
+++ provisioners/run_hw.sh
@@ -5,6 +5,8 @@ hw=$1
 case $hw in
 "hw_01")
   echo "Installation $hw"
+  sudo apt update
+  sudo apt install mc ansible -y
   git clone https://github.com/alexkonkin/gl_hw.git
   cd ./gl_hw/provisioners/$hw
   ansible-playbook --vault-password-file="./vault_pass.txt" --extra-vars="@variables.yml" playbook.yml
