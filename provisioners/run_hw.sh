c6d962911fc5944d7921a5444a25f46b1d5c4ca6
vagrant <vagrant@master1>
2021-05-13 16:02:08 +0000

    allowed ssh auth for master node

--- provisioners/run_hw.sh
+++ provisioners/run_hw.sh
@@ -2,6 +2,14 @@
 
 hw=$1
 
-echo $hw
-
-
+case $hw in
+"hw_01")
+  echo "Installation $hw"
+  git clone https://github.com/alexkonkin/gl_hw.git
+  cd ./gl_hw/provisioners/$hw
+  ansible-playbook --vault-password-file="./vault_pass.txt" --extra-vars="@variables.yml" playbook.yml
+  ;;
+*)
+  echo "incorrect option selected..."
+  ;;
+esac
